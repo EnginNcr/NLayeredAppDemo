44047962758baa8d8d3bb3f984126906eb199a76
TOSHIBA <TOSHIBA@DESKTOP-I7SJKD8>
2022-06-14 19:33:40 +0300

    Add project files.

---
+++ Northwind.Data.Acceses/Concrete/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+
+namespace Northwind.Data.Acceses.Concrete
+{
+    public class NorthwindContext:DbContext
+    {
+        public DbSet<Product> Products{ get; set; }
+    } 
+}
